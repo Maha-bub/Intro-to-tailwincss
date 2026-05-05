9d483135d6fba40eff9d9a9f4151d81baffec605
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-05-05 15:18:27 +0600

    store racatangel width and height data

---
+++ geometry_genius/scripts/Ranctangle_calculation.js
@@ -0,0 +1,17 @@
+function ranctangleAreaCalculation(){
+
+    const RanctangleWidth=document.getElementById('width');
+    const ranctangelvalue=RanctangleWidth.value;
+    const width=parseFloat(ranctangelvalue);
+    // console.log(width);
+
+    const ractangleLengthInput=document.getElementById('length')
+    const rantanglevalue=ractangleLengthInput.value
+    const length=parseFloat(rantanglevalue)
+    console.log(length,width)
+
+
+
+
+
+}
